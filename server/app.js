26eb021da826d28984ce6473048b6470a2b159cf
Vidush2003 <vidushprakashs@gmail.com>
2026-09-24 11:28:41 +0530

    chore: remove temporary seed backdoor

--- server/app.js
+++ server/app.js
@@ -31,7 +31,6 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 app.use('/api/dashboard', require('./routes/dashboardRoutes'));
 app.use('/api/ai', require('./routes/aiRoutes'));
-app.use('/api/seed', require('./routes/seedRoutes'));
 
 // Error handling
 app.use(notFound);
--- server/routes/seedRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { seedRemoteDB } = require('../controllers/seedController');
-
-router.get('/', seedRemoteDB);
-
-module.exports = router;
